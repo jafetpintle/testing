1ab751539f47a69253000496e26a6b42954c5899
jafet <jafetpintle@gmail.com>
2023-02-22 23:02:45 -0600

    Making a setup with before setence

--- src/test/java/Payment/PaymentProcessorTest.java
+++ src/test/java/Payment/PaymentProcessorTest.java
@@ -1,32 +1,32 @@
 package Payment;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
+    PaymentGateway paymentGateway;
+    PaymentProcessor paymentProcessor;
+    @Before
+    public void setUp(){
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
 
     @Test
     public void makePayment_correctPayment(){
-
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         Assert.assertTrue(paymentProcessor.makePayment(1000));
     }
 
     @Test
     public void makePayment_wrongPayment(){
-
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
-
+        
         Assert.assertFalse(paymentProcessor.makePayment(1000));
     }
 
